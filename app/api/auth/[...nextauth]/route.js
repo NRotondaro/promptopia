89a2f31361c3191b3f847a1c38ff7785eee2e3ac
NRotondaro <niksonrotondaro1@gmail.com>
2023-06-15 20:36:49 -0300

    feat: connect db & sign in functionality

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+
+import User from '@models/user';
 import { connectToDB } from '@utils/database';
 
 const handler = NextAuth({
@@ -9,18 +11,39 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
+      });
+      session.user.id = sessionUser._id.toString();
 
-      // check if a user already exists
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
 
-      // if not , create
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+        // check if a user already exists
+        const userExists = await User.findOne({
+          email: profile.email,
+        });
+
+        // if not, create a new user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(' ', '').toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
   },
 });
 
